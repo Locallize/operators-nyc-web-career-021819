cea5fdea1b966f5295912433c0ba3a53ab1de51f
Fillan Lee <fillanlee@aol.com>
2019-02-01 04:52:47 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,14 +3,14 @@ def unsafe?(speed)
     true
   else 40 > speed || speed > 60
     false
-    
-  
+  end
 end
 
 
 
 def not_safe?(speed)
-	speed > 60 ? true : 
+	speed > 60 ? true : 40 > speed || speed > 60
+	                    ? false
 end
 	
 
